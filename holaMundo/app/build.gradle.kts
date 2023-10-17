1432b9aa0ae4daffe0e8df4a40dbe95c22176815
GeraldJT <geraldjt.8@gmail.com>
2023-10-17 06:04:59 -0500

    HOLAMUNDO

--- holaMundo/app/build.gradle.kts
+++ holaMundo/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.holamundo"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.holamundo"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- holaMundo/app/src/main/java/com/example/holamundo/MainActivity.kt
+++ holaMundo/app/src/main/java/com/example/holamundo/MainActivity.kt
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     HolaMundoTheme {
-        Greeting("Android")
+        Greeting("World")
     }
 }
